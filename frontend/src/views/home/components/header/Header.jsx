e4d63f384475374a57885746c905e1dd40692886
shubham <shubhamonline024@gmail.com>
2026-02-22 10:06:48 +0530

    shadcn added to frontend

--- frontend/src/views/home/components/header/Header.jsx
+++ frontend/src/views/home/components/header/Header.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="flex border border-border">
+      <div className="w-4/5 flex justify-start items-center border border-border">
+        Codeshare
+      </div>
+      <div className="w-1/5 flex justify-center items-center border border-border">
+        <Button variant="outline">Logout</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
