97c369460524c28effa9d4307fa54f1246d802a6
shubham <shubhamonline024@gmail.com>
2026-02-22 15:22:44 +0530

    removed legacy frontend

--- frontend/src/views/home/components/footer/Footer.tsx
+++ frontend/src/views/home/components/footer/Footer.tsx
@@ -1,7 +1,10 @@
 const Footer = () => {
   return (
     <div className="w-full flex items-center justify-center bg-(--secondary) h-[5vh] shrink-0 border-0">
-      <p className="text-md text-(--primary) font-semibold">Copyright@2026</p>
+      <p className="text-sm text-(--primary) font-medium">
+        Built By{" "}
+        <a href="https://github.com/shubhamshigwan20">Shubham Shigwan</a>
+      </p>
     </div>
   );
 };
