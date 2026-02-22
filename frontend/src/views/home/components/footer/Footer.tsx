1e03bf3a1bf1469d93183411d3d9011fb1d113e9
shubham <shubhamonline024@gmail.com>
2026-02-22 17:49:05 +0530

    footer changes

--- frontend/src/views/home/components/footer/Footer.tsx
+++ frontend/src/views/home/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="w-full flex items-center justify-center bg-(--secondary) h-[5vh] shrink-0 border-0">
       <p className="text-sm text-(--primary) font-medium">
-        Built By{" "}
+        Made With ❤️ By{" "}
         <a href="https://github.com/shubhamshigwan20">Shubham Shigwan</a>
       </p>
     </div>
